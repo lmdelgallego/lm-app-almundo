a39894bd8723abfea655bfcdb040958f85921122
Luis Miguel Del Gallego Horta <alucardluis@gmail.com>
2018-06-05 03:36:28 -0500

    Clean Code

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ console.log(hostname);
 if(hostname === 'lm-app-almundo.herokuapp.com'){
   backendHost = 'https://lm-api-almundo.herokuapp.com/';
 }else{
-  console.log(process.env.REACT_APP_BACKEND_HOST)
+  console.log(process.env.REACT_APP_BACKEND_HOST);
   backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:1234';
 }
 
